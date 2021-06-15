1c5cb09a424a0db83f18dae3278ecb7ad6866d2c
Thom van der Woude <tbvanderwoude@gmail.com>
2021-06-15 16:45:25 +0200

    Added runner script

---
+++ run.py
@@ -0,0 +1,5 @@
+from python.benchmarks import comparison_ivar_obstacle_1teams, comparison_ivar_obstacle_3teams, comparison_ivar_maze_1teams,comparison_ivar_maze_3teams
+comparison_ivar_obstacle_1teams.main()
+comparison_ivar_obstacle_3teams.main()
+comparison_ivar_maze_1teams.main()
+comparison_ivar_maze_3teams.main()

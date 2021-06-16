3f2c812ebd0caeb7a3c299cd0a1de8a57918d2d6
Thom van der Woude <tbvanderwoude@linux-template-ubuntu-1804-camp.tudelft.nl>
2021-06-16 09:42:54 +0200

    Added results on maze and obstacles

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from python.benchmarks import comparison_ivar_obstacle_1teams, comparison_ivar_obstacle_3teams, comparison_ivar_maze_1teams,comparison_ivar_maze_3teams
 comparison_ivar_obstacle_1teams.main()
 comparison_ivar_obstacle_3teams.main()
